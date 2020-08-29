5da77c7ff23d5254c67593a43c0ab687f7ba6524
Konstantin Alekseev <lexxxlexus@mail.ru>
2020-08-29 20:53:10 +0600

    added slider, styles refactored

--- js/script.js
+++ js/script.js
@@ -5,30 +5,30 @@ $(navlink).on("click", function (event) {
         event.preventDefault();
         menu.toggleClass("show")
     })
-
-
-
     /* Growing block*/
 let button = $("#growing_button")
 let growingItem = $("#growing_item")
 $(button).on("click", function (event) {
-        event.preventDefault();
-        growingItem.toggleClass("hidden")
-        button.toggleClass("reverse")
-    })
+    event.preventDefault();
+    growingItem.toggleClass("hidden")
+    button.toggleClass("reverse")
+})
 
 
+$(function () {
+    let slider = $("#slider");
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: false,
+        arrows: false,
+        dots: true,
+        variableWidth: false,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 1900
+    });
+});
 
 
-
-//$(function () {
-//    let slider = $("#slider");
-//    slider.slick({
-//        infinite: true
-//        , slidesToShow: 2
-//        , slidesToScroll: 2
-//        , fade: true
-//        , arrows: false
-//        , dots: true
-//    , });
-//});
