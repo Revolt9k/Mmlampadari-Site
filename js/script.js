ad44fec614745beb8c43579310c212dfe636d53e
Konstantin Alekseev <lexxxlexus@mail.ru>
2020-08-30 18:08:04 +0600

    added remaining logic, added adaptive styles, added hover effects

--- js/script.js
+++ js/script.js
@@ -1,19 +1,95 @@
-/* DropDown Nav Menu*/
-let navlink = $("#dropdown_link")
-let menu = $("#dropdown_toggle")
-$(navlink).on("click", function (event) {
-        event.preventDefault();
-        menu.toggleClass("show")
-    })
-    /* Growing block*/
-let button = $("#growing_button")
-let growingItem = $("#growing_item")
-$(button).on("click", function (event) {
+/* PopUp*/
+
+let popupbutton = $("#popup_button")
+let popup = $("#popup")
+let popupInner = $("#popup_inner")
+let popupClose = $("#popup_close")
+
+
+$(popupbutton).on("click", function (event) {
     event.preventDefault();
-    growingItem.toggleClass("hidden")
-    button.toggleClass("reverse")
+    popup.toggleClass("open")
+    popupInner.toggleClass("move")
 })
 
+$(popupClose).on("click", function (event) {
+    event.preventDefault();
+    popup.toggleClass("open")
+    popupInner.toggleClass("move")
+})
+
+$(popup).on("click", function (event) {
+    event.preventDefault();
+    if (event.target.className == "popup open") {
+        popup.removeClass("open")
+        popupInner.toggleClass("move")
+    }
+})
+
+
+/* DropDown Nav Menu*/
+
+let navlink = $("#dropdown_link")
+let menu = $("#dropdown_toggle")
+let innermenu = $("#dropdown_inner")
+
+let header = $("#header_inner")
+let main = $("main")
+let footer = $("section")
+
+$(navlink).on("click", function (event) {
+        event.preventDefault();
+        menu.addClass("show")
+        innermenu.addClass("show");
+        event.stopPropagation()
+    })
+
+$(header).on("click", function (event) {
+        event.preventDefault();
+        menu.removeClass("show")
+        innermenu.removeClass("show");
+        burgerMenu.removeClass("toggleburger")
+
+    })
+$(main).on("click", function (event) {
+        event.preventDefault();
+        menu.removeClass("show")
+        innermenu.removeClass("show");
+            burgerMenu.removeClass("toggleburger")
+
+    })
+$(footer).on("click", function (event) {
+        event.preventDefault();
+        menu.removeClass("show")
+        innermenu.removeClass("show");
+        burgerMenu.removeClass("toggleburger")
+        secondBurgerMenu.removeClass("toggleburger")
+    })
+
+
+/* Burger menu*/
+
+let burgerButton = $("#burger_button")
+let burgerMenu = $("#dropdown_burger")
+$(burgerButton).on("click", function (event) {
+        event.preventDefault();
+        burgerMenu.addClass("toggleburger")
+        secondBurgerMenu.removeClass("toggleburger")
+        event.stopPropagation()
+    })
+
+/* Second Burger menu*/
+
+let secondBurgerButton = $("#second_burger_button")
+let secondBurgerMenu = $("#second_dropdown")
+$(secondBurgerButton).on("click", function (event) {
+    event.preventDefault();
+        secondBurgerMenu.addClass("toggleburger")
+        event.stopPropagation()
+    })
+
+
+/* Slider*/
 
 $(function () {
     let slider = $("#slider");
@@ -26,9 +102,22 @@ $(function () {
         dots: true,
         variableWidth: false,
         adaptiveHeight: true,
-        autoplay: true,
-        autoplaySpeed: 1900
+        autoplay: false,
+        autoplaySpeed: 3000
     });
 });
+
+/* Growing block*/
+
+let button = $("#growing_button")
+let growingItem = $("#growing_item")
+$(button).on("click", function (event) {
+    event.preventDefault();
+    growingItem.toggleClass("hidden")
+    button.toggleClass("reverse")
+})
+
+
+
 
 
